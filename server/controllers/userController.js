5f4419d0f103e51747cfbea487e8713909159bb3
mayur-argade <argademayur2002@gmail.com>
2023-10-09 21:57:21 +0530

    code modified

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -5,10 +5,10 @@ const cookieToken = require('../utils/cookieToken')
 const fileUploader = require('express-fileupload')
 const cloudinary = require('cloudinary')
 
-exports.signup = BigPromise(async ( req, res, next) => {
+exports.signup = BigPromise(async (req, res, next) => {
 
-    let result ;
-    if(req.files){
+    let result;
+    if (req.files) {
         let file = req.files.photo
         result = await cloudinary.v2.uploader.upload(file.tempFilePath, {
             folder: "users",
@@ -17,10 +17,10 @@ exports.signup = BigPromise(async ( req, res, next) => {
         })
     }
 
-    const { name, email, password} = req.body
+    const { name, email, password } = req.body
 
-    if(!email || !name || !password){
-        return next(new CustomError('all fields are necessary', 400 ))
+    if (!email || !name || !password) {
+        return next(new CustomError('all fields are necessary', 400))
     }
 
     const user = await User.create({
@@ -37,19 +37,19 @@ exports.signup = BigPromise(async ( req, res, next) => {
 
 })
 
-exports.login = BigPromise(async(req, res, next) => {
-    const {email , password } = req.body
+exports.login = BigPromise(async (req, res, next) => {
+    const { email, password } = req.body
 
     // check for pressence of email and password 
-    if( !email || !password ){
+    if (!email || !password) {
         return next(new CustomError('please provide email and password', 400))
     }
-    
+
     // get user form db
-    const user = await User.findOne({email}).select("+password")
+    const user = await User.findOne({ email }).select("+password")
 
     // if user not found in db
-    if( !user ){
+    if (!user) {
         return next(new CustomError('you are not registered', 400))
     }
 
@@ -57,7 +57,7 @@ exports.login = BigPromise(async(req, res, next) => {
     const isPasswordCorrect = await user.isValidatedPassword(password)
 
     // if password does not match
-    if( !isPasswordCorrect ){
+    if (!isPasswordCorrect) {
         return next(new CustomError('email and password does not match', 400))
     }
 
@@ -66,7 +66,7 @@ exports.login = BigPromise(async(req, res, next) => {
 
 })
 
-exports.logout = BigPromise(async(req, res, next) => {
+exports.logout = BigPromise(async (req, res, next) => {
     res.cookie('token', null, {
         expires: new Date(Date.now()),
         httpOnly: true
@@ -77,8 +77,8 @@ exports.logout = BigPromise(async(req, res, next) => {
     })
 })
 
-exports.getloggedInUserDetails = BigPromise(async(req, res, next) => {
-   
+exports.getloggedInUserDetails = BigPromise(async (req, res, next) => {
+
     const user = await User.findById(req.user.id)
 
     res.status(200).json({
@@ -87,7 +87,7 @@ exports.getloggedInUserDetails = BigPromise(async(req, res, next) => {
     })
 })
 
-exports.adminAllUser = BigPromise(async(req, res, next) => {
+exports.adminAllUser = BigPromise(async (req, res, next) => {
 
     const users = await User.find({})
 
